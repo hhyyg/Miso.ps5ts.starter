ab55d37b16d693c76563e028c33849da875cee8c
hhyyg <hiroka.yago@gmail.com>
2018-09-03 14:39:05 +0900

    init

---
+++ dist/sketch.js
@@ -0,0 +1,11 @@
+"use strict";
+/// <reference path="../types/p5/global.d.ts"/>
+function setup() {
+    createCanvas(600, 600);
+    rectMode(CENTER);
+}
+function draw() {
+    rect(100, 100, 100, 100);
+    rect(100, 300, 20, 20);
+}
+//# sourceMappingURL=sketch.js.map
---
+++ src/sketch.ts
@@ -0,0 +1,12 @@
+
+/// <reference path="../types/p5/global.d.ts"/>
+
+function setup() {
+    createCanvas(600, 600);
+    rectMode(CENTER);
+}
+
+function draw() {
+    rect(100, 100, 100, 100);
+    rect(100, 300, 20, 20);
+}
